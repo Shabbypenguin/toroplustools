6f66eba422ce1033b8c6034e438b2f0966f980f7
David Rouleau <Shabbypenguin@gmail.com>
2012-08-28 20:07:18 -0300

    raided EOS ;P

--- apply.sh
+++ apply.sh
@@ -52,36 +52,25 @@ set -e
 
 ################ Apply Patches Below ####################
 
-repo start auto packages/apps/Settings
-echo "### apply cpufreq patch so processor settings reads our cpufreq table properly... i should commit this to gerrit sometime soon"
-echo "### updated by nivron"
-cdv packages/apps/Settings
-git reset --hard
-http_patch http://chris41g.org/patches/processor.patch
-cdb
-
-##########################################################
-### Cyanogenmod apparently broke shit for these patches###
-##########################################################
-#repo start auto packages/apps/Settings
-#echo "### apply temp fix to lockscreen icons"
-#cdv packages/apps/Settings
-#git reset --hard
-#http_patch http://www.shabbypenguin.com/ACS/devices/Samsung/Sprint/E4GT/Patches/Lockscreen1.patch
-#cdb
-
-#repo start auto frameworks/base
-#echo "### apply temp fix to lockscreen icons"
-#cdv frameworks/base
-#git reset --hard
-#http_patch http://www.shabbypenguin.com/ACS/devices/Samsung/Sprint/E4GT/Patches/Lockscreen2.patch
-#cdb
-
 repo start auto packages/apps/Phone
 echo "### fix ringtones"
 cdv packages/apps/Phone
 git reset --hard
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_packages_apps_Phone refs/changes/12/21512/1 && git cherry-pick FETCH_HEAD
+cdb
+
+repo start auto packages/apps/Phone
+echo "### EOS LTE toggle"
+cdv packages/apps/Phone
+git reset --hard
+git fetch git://git.teameos.org/eos/platform/packages/apps/Phone refs/changes/52/952/2 && git cherry-pick FETCH_HEAD
+cdb
+
+repo start auto packages/apps/Phone
+echo "### EOS - Enable LTE only mode"
+cdv packages/apps/Phone
+git reset --hard
+git fetch git://git.teameos.org/eos/platform/packages/apps/Phone refs/changes/63/963/1 && git cherry-pick FETCH_HEAD
 cdb
 
 ##### SUCCESS ####
